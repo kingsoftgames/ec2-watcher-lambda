46744376b923749215ebf0491c8ffb7c21260a10
Zhiguang Wang <wangzhiguang@kingsoft.com>
2018-09-11 11:48:42 +0800

    Conform to the PEP8 style guide.

--- bearychat.py
+++ bearychat.py
@@ -6,6 +6,21 @@ from botocore.vendored import requests
 BEARYCHAT_WEBHOOK = '<Your BearyChat Webhook>'
 BEARYCHAT_CHANNEL = '<Your BearyChat Channel>'
 
+
+def get_aws_console_domain(region):
+    if region.startswith('cn-'):
+        return 'console.amazonaws.cn'
+    else:
+        return 'console.aws.amazon.com'
+
+
+def get_instance_url(instance_id, region):
+    domain = get_aws_console_domain(region)
+    return (f'https://{domain}/ec2/v2/home'
+            f'?region={region}'
+            f'#Instances:instanceId={instance_id}')
+
+
 def get_instance_name(instance_id, region):
     ec2 = boto3.resource('ec2', region_name=region)
     instance = ec2.Instance(instance_id)
@@ -15,25 +30,28 @@ def get_instance_name(instance_id, region):
             name = tags['Value']
     return name
 
+
 def send_bearychat(payload):
     r = requests.post(BEARYCHAT_WEBHOOK, json=payload)
     return r.text
 
+
 def make_bearychat_payload(event):
     region = event['region']
-    instance_id = event['detail']['instance-id']
-    instance_url = f'https://console.amazonaws.cn/ec2/v2/home?region={region}#Instances:instanceId={instance_id}'
     state = event['detail']['state']
+    instance_id = event['detail']['instance-id']
+    instance_url = get_instance_url(instance_id, region)
     name = get_instance_name(instance_id, region)
     name = f' ({name})' if name else ''
     text = f'EC2 ({region}) **{state}**: [{instance_id}]({instance_url}){name}'
-    # See https://github.com/bearyinnovative/bearychat-docs/blob/master/tutorials/markdown/incoming.md
+    # https://github.com/bearyinnovative/bearychat-docs/blob/master/tutorials/markdown/incoming.md
     payload = dict(
         channel=BEARYCHAT_CHANNEL,
         markdown=True,
         text=text,
     )
     return payload
+
 
 def lambda_handler(event, context):
     payload = make_bearychat_payload(event)
